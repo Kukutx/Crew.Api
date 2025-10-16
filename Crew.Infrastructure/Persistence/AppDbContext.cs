d27a782524822f9c6a2a03133f76313b4289c131
Kukutx <liuzhongli.ascii@gmail.com>
2025-10-17 00:15:51 +0200

    Update Code

--- Crew.Infrastructure/Persistence/AppDbContext.cs
+++ Crew.Infrastructure/Persistence/AppDbContext.cs
@@ -24,5 +24,6 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
